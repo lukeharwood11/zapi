593eaceac37a4e481d026e933c521000e82f5d65
Luke Harwood <lukeharwood11@gmail.com>
2024-10-20 00:28:34 -0500

    Add test

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const net = std.net;
 const http = std.http;
+const fs = std.fs;
 
 const RequestError = error{
     UnsupportedVersion,
@@ -63,19 +64,35 @@ pub fn main() !void {
 
     while (true) {
         const conn = try server.accept();
+        const time_start = std.time.milliTimestamp();
         const writer = conn.stream.writer();
         defer conn.stream.close();
         var buf: [4096]u8 = undefined;
         // read in request from the buffer
         _ = try conn.stream.read(&buf);
         var request = try Request.init(allocator, &buf);
-        std.log.info("The 'Accept' header: '{s}'", .{request.headers.get("Accept").?});
         defer request.deinit();
-        std.log.info("{s} - {s}", .{ @tagName(request.method), request.path });
 
-        const response = "HTTP/1.1 200 OK\r\n\r\n";
+        const response_prefix = "HTTP/1.1 200 OK";
+        // file
+        const file = try fs.cwd().openFile("index.html", .{ .mode = .read_only });
+        defer file.close();
+        // file reader
+        var reader = file.reader();
+        const val = try reader.readAllAlloc(allocator, 8180);
+        defer allocator.free(val);
+        const response = try std.mem.concat(allocator, u8, &.{ (response_prefix ++ "\r\n\r\n")[0..], val });
+        defer allocator.free(response);
         _ = try writer.writeAll(response);
-
-        std.log.info("RESPONSE: {s}", .{response});
+        const time_end = std.time.milliTimestamp();
+        std.log.info("{s} {d:.2}ms - {s} - {s}", .{ @tagName(request.method), time_end - time_start, request.path, response_prefix });
     }
+}
+
+test "request" {
+    const allocator = std.testing.allocator;
+    const test_request = "GET / HTTP/1.1\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en\r\nCache-Control: no-cache\r\nConnection: keep-alive\r\nCookie: _xsrf=2|15cc4973|28fad4828d5481671268275137b8d5ef|1726954072; username-localhost-8889=\"2|1:0|10:1726954159|23:username-localhost-8889|44:YjQ5NWUwNTk0YWYzNDdiYjg0YTUyMjBkZWUzN2QzOWI=|55dcaa14c31839827ce4aadfe1441416a20d4daaa5237352b9978066db7245a2\"; username-localhost-8888=\"2|1:0|10:1729383800|23:username-localhost-8888|44:MDk3ZDk0M2RkOWM4NGE5MGJiZjY2MTkzOWI5Yzc2ZmI=|afe15b44875b430af63e67ff9d5430c03a83d149cf7f090889dd9f8e307e43bb\"\r\nHost: localhost:8080\r\nPragma: no-cache\r\nSec-Fetch-Dest: document\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-Site: none\r\nSec-Fetch-User: ?1\r\nSec-GPC: 1\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36\r\nsec-ch-ua: \"Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Brave\";v=\"122\"\r\nsec-ch-ua-mobile: ?0\r\nsec-ch-ua-platform: \"macOS\"\r\n\r\n";
+    var request = try Request.init(allocator, test_request);
+    defer request.deinit();
+    try std.testing.expect(std.mem.eql(u8, "/", request.path));
 }
